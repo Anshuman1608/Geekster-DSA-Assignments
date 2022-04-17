6b6ae3fbd1a6c927fd91e82a0c1028dd247e11d2
Anshuman Singh <anshumaansingh98@gmail.com>
2022-04-17 19:07:16 +0530

    DSA Questions

---
+++ 11 April 2022-17 April 2022/11 April 2022/MissingElement.java
@@ -0,0 +1,21 @@
+package March;
+
+public class MissingElement {
+    static int getMissingNo(int a[], int n)
+    {
+        int total = 1;
+        for (int i = 2; i <= (n + 1); i++)
+        {
+            total += i;
+            total -= a[i - 2];
+        }
+        return total;
+    }
+
+
+    public static void main(String[] args)
+    {
+        int[] arr = { 1, 2, 3, 5 };
+        System.out.println(getMissingNo(arr, arr.length));
+    }
+}
